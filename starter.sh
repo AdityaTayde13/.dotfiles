194ee3c00d17a1a7fe18a18b7b484db46b437a87
AdityaTayde13 <taydeaditya52@gmail.com>
2023-08-12 18:12:29 +0000

    updated starter.sh

--- starter.sh
+++ starter.sh
@@ -6,6 +6,4 @@
 ## install neovim zsh , ripgrep , fd-find, gcc for tree sitter , git
 mkdir ~/tmp
 chsh -s /usr/bin/zsh
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/tmp/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/tmp/powerlevel10k
 git clone https://github.com/AdityaTayde13/.dotfiles.git

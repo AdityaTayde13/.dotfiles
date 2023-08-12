0cfaa3a617206d03d7e9394dd5cd0f9ce3dfb16e
AdityaTayde13 <taydeaditya52@gmail.com>
2023-08-12 19:00:16 +0000

    updated starter.sh

--- starter.sh
+++ starter.sh
@@ -2,6 +2,8 @@
 
 ### run this file when setting up a new distro
 
+# link to guide for managing dotfiles https://www.anand-iyer.com/blog/2018/a-simpler-way-to-manage-your-dotfiles.html
+
 ## set font to fira code nerd font
 ## install neovim zsh , ripgrep , fd-find, gcc for tree sitter , git
 mkdir ~/tmp
